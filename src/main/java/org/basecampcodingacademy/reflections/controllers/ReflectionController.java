d1e4420e6a4f42325a2f14f4083595b0ca955b70
Soledad Unda <51492868+ASoledadU@users.noreply.github.com>
2020-03-26 09:24:37 -0500

    Update Reflections
    Updating all the files from my local machine to GitHub one file at a time.

--- src/main/java/org/basecampcodingacademy/reflections/controllers/ReflectionController.java
+++ src/main/java/org/basecampcodingacademy/reflections/controllers/ReflectionController.java
@@ -1,19 +1,28 @@
 package org.basecampcodingacademy.reflections.controllers;
 
+import org.apache.tomcat.jni.Local;
+import org.basecampcodingacademy.reflections.db.QuestionRepository;
 import org.basecampcodingacademy.reflections.db.ReflectionRepository;
 import org.basecampcodingacademy.reflections.domain.Reflection;
+import org.basecampcodingacademy.reflections.exceptions.OneReflectionPerDay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/reflections")
 public class ReflectionController {
     @Autowired
     public ReflectionRepository reflections;
+
+    @Autowired
+    public QuestionRepository questions;
 
     @GetMapping
     public List<Reflection> index() {
@@ -22,13 +31,20 @@ public class ReflectionController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Reflection create(@RequestBody Reflection reflection) {
-        return reflections.create(reflection);
+    public Reflection create(@RequestBody Reflection reflection) throws OneReflectionPerDay {
+        if (Objects.isNull(reflections.find(reflection.date))) {
+            return reflections.create(reflection);
+        }
+        throw new OneReflectionPerDay(reflection.date);
     }
 
+
     @GetMapping("/today")
-    public Reflection today() {
+    public Reflection today(@RequestParam(defaultValue= "") String include) {
         var reflection =  reflections.find(LocalDate.now());
+        if (include.equals("questions")) {
+            reflection.questions = questions.forReflection(reflection.id);
+        }
         return reflection;
     }
 
@@ -42,5 +58,13 @@ public class ReflectionController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
         reflections.delete(id);
+    }
+
+    @ExceptionHandler ({ OneReflectionPerDay.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleOneReflectionPerDayException(OneReflectionPerDay ex) {
+        var errorMap = new HashMap<String, String>();
+        errorMap.put("error", "Reflection for " + ex.date.toString() + " already exists");
+        return errorMap;
     }
 }
